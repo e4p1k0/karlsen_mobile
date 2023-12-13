22bd2c006af9e5ce29f73653892872c3583c4c12
btclinux <btclinux@gmail.com>
2023-12-13 16:34:55 +0300

    fix

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,52 +1,52 @@
 import 'package:flutter/material.dart';
-pp_af.arb
-pp_af.arb
-pp_af.arb
+
+
+
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-pp_af.arb
-pp_af.arb
-pp_af.arb
 
-pp_af.arb
-pp_af.arb
-pp_af.arb
+
+
+
+
+
+
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
-pp_af.arb
-pp_af.arb
-pp_af.arb
 
-pp_af.arb
-pp_af.arb
-pp_af.arb
+
+
+
+
+
+
 AppLocalizations l10nOf(BuildContext context) => AppLocalizations.of(context)!;
-pp_af.arb
-pp_af.arb
-pp_af.arb
 
-pp_af.arb
-pp_af.arb
-pp_af.arb
+
+
+
+
+
+
 class L10nWrapper {
-pp_af.arb
-pp_af.arb
-pp_af.arb
-  AppLocalizations? l10n;
-pp_af.arb
-pp_af.arb
-pp_af.arb
-  L10nWrapper();
-pp_af.arb
-pp_af.arb
-pp_af.arb
-}
-pp_af.arb
-pp_af.arb
-pp_af.arb
 
-pp_af.arb
-pp_af.arb
-pp_af.arb
+
+
+  AppLocalizations? l10n;
+
+
+
+  L10nWrapper();
+
+
+
+}
+
+
+
+
+
+
+
 final l10nWrapper = L10nWrapper();
-pp_af.arb
-pp_af.arb
-pp_af.arb
+
+
+
